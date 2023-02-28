445f7ec02e806e3bedcbb34cc4587a56d54fd34a
Moh20All <lotfiallali222@gmail.com>
2023-02-28 20:38:20 +0100

    Nrml

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let myElem = document.getElementById("srb");
+let cas = 0;
+function appear() {
+
+    myElem.classList.add("active-sr");
+
+}
+function disaper() {
+    myElem.classList.remove("active-sr");
+}
