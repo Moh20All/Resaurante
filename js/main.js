6a41222900c67da1a5d02f9da3c83d25d32b68de
Moh20All <lotfiallali222@gmail.com>
2023-03-03 23:56:41 +0100

    spicy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let myElem = document.getElementById("srb");
 let cas = 0;
+let list = document.querySelectorAll('.nav');
+console.log(list[0].classList.contains("active-ls"));
+
 function appear() {
 
     myElem.classList.add("active-sr");
@@ -7,4 +10,12 @@ function appear() {
 }
 function disaper() {
     myElem.classList.remove("active-sr");
+}
+
+function listappear() {
+    if (list[0].classList.contains("active-ls")) {
+        list[0].classList.remove("active-ls");
+    } else {
+        list[0].classList.add("active-ls");
+    }
 }
