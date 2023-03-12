3e45854d07d15516424e7e6245dc4580b3ca4dd4
Moh20All <lotfiallali222@gmail.com>
2023-03-13 00:01:52 +0100

    GG

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let myElem = document.getElementById("srb");
 let cas = 0;
 let list = document.querySelectorAll('.nav');
 let menu = document.querySelectorAll('.dishes-container');
+let ico = document.getElementById("drop-ic");
+let drop = document.getElementById("menu");
 
 function appear() {
 
@@ -34,4 +36,10 @@ function menu3() {
     menu[2].classList.add('active-menu');
     menu[1].classList.remove('active-menu')
     menu[0].classList.remove('active-menu')
+}
+function drop1() {
+    if (!drop.classList.contains("inactive")) {
+        console.log(drop);
+        drop.style.display = 'none'
+    }
 }
